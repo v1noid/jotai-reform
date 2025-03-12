34ef1b18974e44c53ce67f0cd3cb14ca5297f3ff
sirarifarid <arifmp40@gmail.com>
2025-03-12 22:46:17 +0600

    chore: update jotai-reform dependency to version 1.0.13; bump package version to 1.0.15; adjust import paths and initial state values

--- test/app/page.tsx
+++ test/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { createAtom } from "@/dist";
+import { createAtom } from "jotai-reform";
 import { useAtom } from "jotai/react";
 import { useHydrateAtoms } from "jotai/utils";
 import { useEffect } from "react";
@@ -10,13 +10,12 @@ const [useAtom_, aom] = createAtom({ count: 0 }, (set) => ({
 }));
 
 export default function Home() {
-  useHydrateAtoms([[aom, { count: 10 }]]);
+  useHydrateAtoms([[aom, { count: 300 }]]);
 
   const [count_, methods, setA] = useAtom_();
   const [count, setAtom] = useAtom(aom);
   // using import in dist/index.js gives {count:1000} {count:1000} which is correct but if i use require() it gives {count:100} {count:1000} which is wrong why is that
   console.log(count, count_);
-  methods.inc();
   useEffect(() => {
     setAtom({ count: 100 });
     setA({ count: 1000 });
